5a8ce868e0191c54bccbb211abc9b18980c71ca5
Alleo Indong <freedom_war550@live.com.ph>
2014-10-19 11:27:22 +0800

    Initial project commit

---
+++ CSharpCRUDExample/App/frm_Main.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace CSharpCRUDExample.App
+{
+    partial class frm_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 443);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CSharp CRUD Example by Zomni Labs";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
